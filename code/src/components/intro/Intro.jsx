e91f86779c7bf76cd5744e26e83b6d50925ceed3
silvia <silvia@bridge.ufsc.br>
2021-12-18 11:25:50 -0300

    #8 right on master hehe

--- code/src/components/intro/Intro.jsx
+++ code/src/components/intro/Intro.jsx
@@ -27,7 +27,7 @@ const Intro = () => {
                             Creative and passionate about nature, the human brain and everything in between. Everything.
                         </p>
                         <div> 
-                            <a href="http://silvialaurentino.github.io/" target="_blank"><img src="https://img.shields.io/badge/Portfolio-%23333?style=for-the-badge&logo=github&logoColor=white&color=9cf" target="_blank"/></a>
+                            <a href="https://github.com/silvialaurentino" target="_blank"><img src="https://img.shields.io/badge/GitHub-%23333?style=for-the-badge&logo=github&logoColor=white&color=9cf" target="_blank"/></a>
                             <a href="https://silvialaurentino.substack.com/" target="_blank"><img src="https://img.shields.io/badge/Newsletter-%23333?style=for-the-badge&logo=substack&logoColor=white&color=9cf" target="_blank"/></a>
                             <a href="https://www.flickr.com/photos/ketolow/" target="_blank"><img src="https://img.shields.io/badge/Flickr-%23333?style=for-the-badge&logo=flickr&logoColor=white&color=9cf" target="_blank"/></a> 
                             <a href = "mailto:silvialaurentinc@gmail.com"><img src="https://img.shields.io/badge/-Gmail-%23333?style=for-the-badge&logo=gmail&logoColor=white&color=9cf"/></a>
